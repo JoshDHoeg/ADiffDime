d0e87f5cda7f8f5fd3324396bb12fb42ca60ff45
Josh Hoeg <jdhoeg97@gmail.com>
2017-07-31 22:18:27 -0600

    trying out a different version of the thumbnails

--- content.php
+++ content.php
@@ -1,31 +1,27 @@
 <!--content template how a particular content item appears in the website-->
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('container col-xs-12 latest-posts-item shadow'); ?>>
-		<?php if( has_post_thumbnail() ): ?>
-			<div class="latest-posts-item-content">
-				<a href="" data-toggle="modal" data-target="#post-detail-<?php the_ID(); ?>">
-					<div class="thumbnail-title">
-						<h1>
-							<!-- <?php the_title() ?> -->
-							<?php
-								$thetitle = $post->post_title; /* or you can use get_the_title() */
-								$getlength = strlen($thetitle);
-								$thelength = 64;
-								echo substr($thetitle, 0, $thelength);
-							?>
-						</h1>
-					</div>
-					<div class="thumbnail-image container col-xs-12">
-						<div class="thumbnail">
-							<?php the_post_thumbnail('small'); ?>
-						</div>
-					</div>
-				</a>
+	<?php
+		if( has_post_thumbnail() ):
+		$urlImg = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+		endif;
+	?>
+		<a class="latest-posts-item-link" href="" data-toggle="modal" data-target="#post-detail-<?php the_ID(); ?>">
+			<div class="latest-posts-item-content" style="background-image: url(<?php echo $urlImg; ?>);">
+				<div class="latest-post-title">
+					<h1>
+						<!-- <?php
+							$thetitle = $post->post_title;
+							$getlength = strlen($thetitle);
+							$thelength = 64;
+							echo substr($thetitle, 0, $thelength);
+						?> -->
+						<?php the_title() ?>
+					</h1>
+				</div>
 			</div>
-		<?php else: ?>
-			<!-- <div class="thumbnail"><a href="" data-toggle="modal" data-target="#post-detail-<?php the_ID(); ?>"><h1><?php the_title() ?></h1><?php the_post_thumbnail('small'); ?></a></div> -->
-			<!--need to add a temporary thumbnail-->
-		<?php endif; ?>
+		</a>
+		
 
 		<!--boostrap modal container-->
 		<div class="modal fade <?php post_class('container post-modal'); ?>" id="post-detail-<?php the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="purchaseLabel" aria-hidden="true">
