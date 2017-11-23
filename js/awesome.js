13a52112be01a80585c41241af240cef26f9845e
Josh Hoeg <jdhoeg97@gmail.com>
2017-11-23 21:55:53 +0100

    updated the menu to be hidden when scrolling

--- js/awesome.js
+++ js/awesome.js
@@ -1,10 +1,50 @@
 //javascript functions
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+
 
 // site load_screen -- also uncomment the div in the header and the css style for #load_screen
 jQuery(document).ready(function($) {
+var navbarHeight = $('#sidenav-menus').outerHeight();
+
   $(window).load(function(){
     $('#load_screen').fadeOut('slow',function(){$(this).remove();});
   });
+
+  //on scroll, let the interval function know the user has scrolled
+  $(window).scroll(function(){
+    didScroll = true;
+  });
+
+  // run hasScrolled() and reset didScroll status
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    if (Math.abs(lastScrollTop - st) <= delta){
+      return;
+    }
+    // If current position > last position AND scrolled past navbar...
+    if (st > lastScrollTop && st > navbarHeight){
+    // Scroll Down
+      $('#sidenav-menus').addClass('nav-up').removeClass('nav-downp');
+    } else {
+      // Scroll Up
+      console.log("weh");
+      // If did not scroll past the document (possible on mac)...
+      if(st + $(window).height() < $(document).height()) {
+        $('#sidenav-menus').removeClass('nav-up').addClass('nav-down');
+      }
+    }
+    lastScrollTop = st;
+
+  };
 });
 
 
@@ -44,7 +84,5 @@ function toggleSidenav(menu) {
   contentBox.checked = false;
   shopBox.checked = false;
   servicesBox.checked = false;
-
-
 
 };
